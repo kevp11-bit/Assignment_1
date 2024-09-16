778346d4f16bc0c27e4610c573afc0b9cca34b6c
mazzas1 <mazzas1@newpaltz.edu>
2024-09-16 14:05:37 -0400

    Update InputOutputPrototype.java

--- User_CompEng_API/InputOutputPrototype.java
+++ User_CompEng_API/InputOutputPrototype.java
@@ -1,8 +1,8 @@
 
 public class InputOutputPrototype{
-	public void GetSources(InputRequest source, OutputRequest destination, String delimiter){	//user submits input source, output destination, delimiter
+	public void getSources(InputRequest source, OutputRequest destination, String delimiter){	//user submits input source, output destination, delimiter
 		//source of data
-		InputRequest WhereFrom = source;
+		InputRequest whereFrom = source;
 		
 		if(delimiter == null || delimiter.isEmpty()){ //if the user doesn't provide a delimiter, use this
 			delimiter = "/";
